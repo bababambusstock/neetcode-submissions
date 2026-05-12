7c249bfbc9e0a78b124b60dcda5b22ee0e1feb11
bababambusstock <147264071+bababambusstock@users.noreply.github.com>
2026-05-12 16:01:07 +0200

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int n = nums.Length;
+        int[] ans = new int[n];
+
+        ans[0] = 1;
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+
+            ans[i] = ans[i] * rightProduct; 
+            
+            rightProduct = rightProduct * nums[i]; 
+        }
+
+        return ans;
+    }
+}
